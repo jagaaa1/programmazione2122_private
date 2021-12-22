c8d02bd192ed760c39b3109ce765772bf7e2b719
Jacopo <jaga.jacopo@gmail.com>
2021-12-22 15:44:04 +0100

    commit esercizio

---
+++ Esercitazione 14-12/shared.rkt
@@ -0,0 +1,25 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname shared) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(define is-in?
+	(lambda (n s)
+		(cond
+			((null? s) false)
+			((= n (car s)) true)
+			(else 
+				(is-in? n (cdr s))
+			)
+		)
+	)
+)
+
+(define shared
+	(lambda (s1 s2)
+		(cond
+			((or (null? s1) (null? s2)) null)
+			((is-in? (car s1) s2) (cons (car s1) (shared (cdr s1) s2)))
+			(else (shared (cdr s1) s2))
+		)
+	)
+)
